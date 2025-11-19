df9ff027366d401356e3584e217bc760ecbf75b0
Chuseok22 <bjh59629@naver.com>
2025-11-19 11:52:28 +0900

    로그인_버튼_컴포넌트_추가 : feat : 로그인 버튼 컴포넌트 추가 https://github.com/CampusTable/campus-table-fe/issues/2

---
+++ src/featrues/auth/components/LoginButton.tsx
@@ -0,0 +1,22 @@
+import { JSX } from "react";
+import styles from "./login-button.module.css"
+
+interface LoginButtonProps {
+  label?: string; // 버튼 라벨
+  disabled?: boolean; // 활성화/비활성화
+}
+
+export default function LoginButton({
+  label = "로그인",
+  disabled = false,
+}: LoginButtonProps): JSX.Element {
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      className={styles.button}
+    >
+      <span className={styles.label}>{label}</span>
+    </button>
+  );
+}
