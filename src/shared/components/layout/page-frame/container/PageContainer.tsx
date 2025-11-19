1c520b19b7885cc7abd21d26e42a28b84e81c3d3
Chuseok22 <bjh59629@naver.com>
2025-11-19 13:24:13 +0900

    로그인_버튼_컴포넌트_추가 : feat : import 세미콜론 추가 https://github.com/CampusTable/campus-table-fe/issues/2

--- src/shared/components/layout/page-frame/container/PageContainer.tsx
+++ src/shared/components/layout/page-frame/container/PageContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./page-container.module.css"
+import styles from "./page-container.module.css";
 
 export default function PageContainer({
   children
