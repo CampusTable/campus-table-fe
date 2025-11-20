6f24c0e5f1d20b2762d12acb1c54bc75d432b898
Chuseok22 <bjh59629@naver.com>
2025-11-20 15:22:02 +0900

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#f55b03",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "/icons/icon-192x192.png",
