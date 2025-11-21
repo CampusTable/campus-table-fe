75862bcac7aeb1ec48355e6f2afa683ef744b225
Chuseok22 <bjh59629@naver.com>
2025-11-21 17:14:28 +0900

    로그인_페이지_UI_개발 : feat : LoginForm 에러 문구 컴포넌트 추가 https://github.com/CampusTable/campus-table-fe/issues/34

---
+++ src/features/auth/components/label/LoginFormErrorLabel.tsx
@@ -0,0 +1,22 @@
+import { ErrorCircleFilled } from "@/assets/icons";
+import styles from "./login-form-error-label.module.css";
+
+interface LoginFormErrorLabelProps {
+  visible?: boolean;
+}
+
+export default function LoginFormErrorLabel({
+  visible = false,
+}: LoginFormErrorLabelProps) {
+
+  const containerClassName: string = visible
+    ? styles.container
+    : styles.containerHidden;
+
+  return (
+    <div className={containerClassName}>
+      <ErrorCircleFilled />
+      <span className={styles.label}>학번 및 비밀번호를 다시 확인하세요</span>
+    </div>
+  );
+}
