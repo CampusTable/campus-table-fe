7d421210f1fcf0394cdf4fec9793424fb375295a
Chuseok22 <bjh59629@naver.com>
2025-11-21 14:38:58 +0900

    로그인_버튼_로딩_애니메이션_적용 : feat : 로그인 버튼 loading 상태 추가 https://github.com/CampusTable/campus-table-fe/issues/30

--- src/features/auth/components/button/LoginButton.tsx
+++ src/features/auth/components/button/LoginButton.tsx
@@ -1,21 +1,25 @@
 import styles from "./login-button.module.css";
+import LoadingDots from "@/shared/components/loading/dot/LoadingDots";
 
 interface LoginButtonProps {
   label?: string; // 버튼 라벨
   disabled?: boolean; // 활성화/비활성화
+  loading?: boolean; // 로딩 중
 }
 
 export default function LoginButton({
   label = "로그인",
   disabled = false,
+  loading = false,
 }: LoginButtonProps) {
+  const isDisabled: boolean = disabled || loading;
   return (
     <button
       type="submit"
-      disabled={disabled}
+      disabled={isDisabled}
       className={styles.button}
     >
-      <span className={styles.label}>{label}</span>
+      {loading ? <LoadingDots /> : <span className={styles.label}>{label}</span>}
     </button>
   );
 }
