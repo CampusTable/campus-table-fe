fceede000e5a5301431d460cc345c42b4a0a975c
Chuseok22 <bjh59629@naver.com>
2025-11-23 20:30:21 +0900

    광고_배너_컴포넌트_개발 : feat : 학생회관 페이지 임시 광고 배너 컴포넌트 추가 https://github.com/CampusTable/campus-table-fe/issues/47

--- src/app/(main)/hakgwan/page.tsx
+++ src/app/(main)/hakgwan/page.tsx
@@ -1,5 +1,10 @@
+import AdBannerCarousel from "@/features/banner/components/AdBannerCarousel";
+
 export default function HakgwanMainPage() {
   return (
-    <div>학생회관 메인 페이지</div>
+    <>
+      <div>학생회관 메인 페이지</div>
+      <AdBannerCarousel />
+    </>
   );
 };
