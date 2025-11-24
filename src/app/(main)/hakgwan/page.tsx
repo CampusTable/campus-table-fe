b7777d899a0ad7499e7d17960373bb745ab65e54
Chuseok22 <bjh59629@naver.com>
2025-11-24 09:50:33 +0900

    인기태그_컴포넌트_추가 : feat : 인기 태그 페이지 임시 적용 https://github.com/CampusTable/campus-table-fe/issues/54

--- src/app/(main)/hakgwan/page.tsx
+++ src/app/(main)/hakgwan/page.tsx
@@ -1,10 +1,17 @@
 import AdBannerCarousel from "@/features/banner/components/AdBannerCarousel";
+import PopularTag from "@/features/menu/tags/components/PopularTag";
 
 export default function HakgwanMainPage() {
   return (
     <>
       <div>학생회관 메인 페이지</div>
       <AdBannerCarousel />
+      <div className="h-10"/>
+      <PopularTag rank={1}/>
+      <div className="h-10"/>
+      <PopularTag rank={2}/>
+      <div className="h-10"/>
+      <PopularTag rank={3}/>
     </>
   );
 };
