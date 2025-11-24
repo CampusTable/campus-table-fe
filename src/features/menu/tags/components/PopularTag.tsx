70d914f78180f5fdc6424235ecbe500d4fcff723
Chuseok22 <bjh59629@naver.com>
2025-11-24 09:50:21 +0900

    인기태그_컴포넌트_추가 : feat : 인기태그 컴포넌트 추가 https://github.com/CampusTable/campus-table-fe/issues/54

---
+++ src/features/menu/tags/components/PopularTag.tsx
@@ -0,0 +1,20 @@
+import styles from "./popular-tag.module.css";
+
+type Rank = 1 | 2 | 3;
+
+interface PopularTagProps {
+  rank: Rank;
+}
+
+export default function PopularTag({
+  rank,
+}: PopularTagProps) {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.label}>
+        인기 {rank}위
+      </div>
+    </div>
+  );
+};
