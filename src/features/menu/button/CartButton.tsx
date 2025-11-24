080c3ce1710fe4ad0e6968b848c5134610dafb8e
Chuseok22 <bjh59629@naver.com>
2025-11-24 17:07:52 +0900

    주문_Bar_컴포넌트_개발 : feat : 장바구니 버튼 컴포넌트 추가 https://github.com/CampusTable/campus-table-fe/issues/65

---
+++ src/features/menu/button/CartButton.tsx
@@ -0,0 +1,55 @@
+import React, { ComponentType, MouseEventHandler } from "react";
+import styles from "./cart-button.module.css";
+import {
+  RoundedNumber1,
+  RoundedNumber2,
+  RoundedNumber3,
+  RoundedNumber4,
+  RoundedNumber5,
+  RoundedNumber6,
+  RoundedNumber7,
+  RoundedNumber8,
+  RoundedNumber9
+} from "@/assets/icons";
+
+interface CartButtonProps {
+  itemCount: ItemCount;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export type ItemCount = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+const IconMap: Record<ItemCount, ComponentType<React.SVGProps<SVGSVGElement>>> = {
+  1: RoundedNumber1,
+  2: RoundedNumber2,
+  3: RoundedNumber3,
+  4: RoundedNumber4,
+  5: RoundedNumber5,
+  6: RoundedNumber6,
+  7: RoundedNumber7,
+  8: RoundedNumber8,
+  9: RoundedNumber9,
+};
+
+export default function CartButton({
+  itemCount,
+  onClick,
+}: CartButtonProps) {
+
+  const Icon = IconMap[itemCount];
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={styles.button}
+    >
+      <div className={styles.labelWrapper}>
+        <Icon className={styles.icon} />
+        <div className={styles.label}>
+          장바구니 보기
+        </div>
+      </div>
+    </button>
+  );
+};
