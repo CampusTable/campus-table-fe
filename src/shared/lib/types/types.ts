d4e06b003978a07ea21dc5c3f28c07b6906707be
Chuseok22 <bjh59629@naver.com>
2025-11-27 13:30:30 +0900

    API_기본_설정_및_로그인_API_연결 : feat : 환경변수 URL 설정 https://github.com/CampusTable/campus-table-fe/issues/75

---
+++ src/shared/lib/types/types.ts
@@ -0,0 +1,12 @@
+const ORIGIN_URL: string = process.env.ORIGIN_URL ?? "";
+const API_BASE_URL: string = process.env.API_BASE_URL ?? "";
+
+if (!ORIGIN_URL) {
+  throw new Error("ORIGIN_URL 환경변수가 설정되지 않았습니다.");
+}
+
+if (!API_BASE_URL) {
+  throw new Error("API_BASE_URL 환경변수가 설정되지 않았습니다.");
+}
+
+export { ORIGIN_URL, API_BASE_URL };
