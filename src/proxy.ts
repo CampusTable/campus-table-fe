4420d5443832ee5d3ca87ac71beee802df8dcb69
Chuseok22 <bjh59629@naver.com>
2025-11-27 16:26:17 +0900

    API_기본_설정_및_로그인_API_연결 : feat : 임시 proxy.ts 동작 해제 https://github.com/CampusTable/campus-table-fe/issues/75

--- src/proxy.ts
+++ src/proxy.ts
@@ -34,41 +34,44 @@ function isAuthOnlyPage(pathname: string): boolean {
 }
 
 export function proxy(request: NextRequest): NextResponse {
-  const { pathname } = request.nextUrl;
+  return NextResponse.next(); // FIXME: 추후 백엔드 엑세스 토큰 발급 로직 완성 후 삭제
 
-  // 정적 리소스 통과
-  if (isStaticAsset(pathname)) {
-    return NextResponse.next();
-  }
-
-  // API 요청 통과
-  if (pathname.startsWith("/api/")) {
-    return NextResponse.next();
-  }
-
-  const accessTokenCookie = request.cookies.get(ACCESS_TOKEN_COOKIE_NAME);
-  const hasAccessToken: boolean = Boolean(accessTokenCookie?.value);
-
-  const isWhitelistedPath: boolean = isAuthWhitelistPath(pathname);
-  const isAuthPagePath: boolean = isAuthOnlyPage(pathname);
-
-  // 로그인 X, 인증이 필요한 경로 접근 -> /login 리다이렉트
-  if (!hasAccessToken && !isWhitelistedPath) {
-    const loginUrl: URL = new URL("/login", request.url);
-    const callbackPath: string = `${pathname}${request.nextUrl.search}`;
-    if (callbackPath !== "/") {
-      loginUrl.searchParams.set("callbackUrl", callbackPath);
-    }
-    return NextResponse.redirect(loginUrl);
-  }
-
-  // 이미 로그인된 사용자가 AUTH_PAGE 접근 시 -> 루트(/) 라다이렉트
-  if (hasAccessToken && isAuthPagePath) {
-    const rootUrl: URL = new URL("/", request.url);
-    return NextResponse.redirect(rootUrl);
-  }
-
-  return NextResponse.next();
+  // TODO: 주석 해제
+  // const { pathname } = request.nextUrl;
+  //
+  // // 정적 리소스 통과
+  // if (isStaticAsset(pathname)) {
+  //   return NextResponse.next();
+  // }
+  //
+  // // API 요청 통과
+  // if (pathname.startsWith("/api/")) {
+  //   return NextResponse.next();
+  // }
+  //
+  // const accessTokenCookie = request.cookies.get(ACCESS_TOKEN_COOKIE_NAME);
+  // const hasAccessToken: boolean = Boolean(accessTokenCookie?.value);
+  //
+  // const isWhitelistedPath: boolean = isAuthWhitelistPath(pathname);
+  // const isAuthPagePath: boolean = isAuthOnlyPage(pathname);
+  //
+  // // 로그인 X, 인증이 필요한 경로 접근 -> /login 리다이렉트
+  // if (!hasAccessToken && !isWhitelistedPath) {
+  //   const loginUrl: URL = new URL("/login", request.url);
+  //   const callbackPath: string = `${pathname}${request.nextUrl.search}`;
+  //   if (callbackPath !== "/") {
+  //     loginUrl.searchParams.set("callbackUrl", callbackPath);
+  //   }
+  //   return NextResponse.redirect(loginUrl);
+  // }
+  //
+  // // 이미 로그인된 사용자가 AUTH_PAGE 접근 시 -> 루트(/) 라다이렉트
+  // if (hasAccessToken && isAuthPagePath) {
+  //   const rootUrl: URL = new URL("/", request.url);
+  //   return NextResponse.redirect(rootUrl);
+  // }
+  //
+  // return NextResponse.next();
 }
 
 
