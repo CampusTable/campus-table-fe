c6963fa64287f76c48bea2349d5a74c6bc824934
Chuseok22 <bjh59629@naver.com>
2025-11-28 08:48:46 +0900

    hotfix(env): env 환경변수 브라우저에서 사용 가능하도록 NEXT_PUBLIC prefix 추가

--- src/shared/lib/types/envUrls.ts
+++ src/shared/lib/types/envUrls.ts
@@ -1,5 +1,5 @@
-const ORIGIN_URL: string = process.env.ORIGIN_URL ?? "";
-const API_BASE_URL: string = process.env.API_BASE_URL ?? "";
+const ORIGIN_URL: string = process.env.NEXT_PUBLIC_ORIGIN_URL ?? "";
+const API_BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
 
 if (!ORIGIN_URL) {
   throw new Error("ORIGIN_URL 환경변수가 설정되지 않았습니다.");
