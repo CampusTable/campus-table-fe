29fd076568e88ce176cbc5cb3115b60de1c84616
Chuseok22 <bjh59629@naver.com>
2025-11-28 13:53:25 +0900

    헤더_네비바_적용에_따른_동적_layout_구조_변경 : feat : 헤더, 네비바 기준 PageView 코드 수정 https://github.com/CampusTable/campus-table-fe/issues/81

--- src/shared/components/layout/page-frame/view/PageView.tsx
+++ src/shared/components/layout/page-frame/view/PageView.tsx
@@ -1,12 +1,34 @@
 import styles from "./PageView.module.css";
 
+export type PageViewVariant = "plain" | "header-only" | "header-nav";
+
+interface PageViewProps {
+  children: React.ReactNode;
+  variant?: PageViewVariant;
+  className?: string;
+}
+
 export default function PageView({
-  children
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+  children,
+  variant = "plain",
+  className,
+}: Readonly<PageViewProps>) {
+  let containerClassName: string = styles.container;
+
+  if (variant === "plain") {
+    containerClassName = `${styles.container} ${styles.containerPlain}`;
+  } else if (variant === "header-only") {
+    containerClassName = `${styles.container} ${styles.containerHeaderOnly}`;
+  } else if (variant === "header-nav") {
+    containerClassName = `${styles.container} ${styles.containerHeaderNav}`;
+  }
+
+  if (className) {
+    containerClassName = `${containerClassName} ${className}`;
+  }
+
   return (
-    <main className={styles.container}>
+    <main className={containerClassName}>
       {children}
     </main>
   );
