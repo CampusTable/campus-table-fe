d69ae8ba70ee3bda1da828916685b424f048f950
Chuseok22 <bjh59629@naver.com>
2025-12-11 15:56:55 +0900

    학생회관_식당_api_연동 : feat : api 및 컴포넌트 props types 정리 https://github.com/CampusTable/campus-table-fe/issues/106

---
+++ src/features/menu/types/categoryType.ts
@@ -0,0 +1,10 @@
+export interface CategoryApiResponse {
+  categoryId: number;
+  categoryName: string;
+  cafeteriaId: number;
+}
+
+export interface CategoryItem {
+  id: number;
+  label: string;
+}
