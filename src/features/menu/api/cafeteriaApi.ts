f86fb7a65386e731a52f316debca9d9feafbecea
Chuseok22 <bjh59629@naver.com>
2025-12-11 15:57:29 +0900

    학생회관_식당_api_연동 : feat : 백엔드 api 호출 로직 추가 https://github.com/CampusTable/campus-table-fe/issues/106

---
+++ src/features/menu/api/cafeteriaApi.ts
@@ -0,0 +1,6 @@
+import { apiServer } from "@/shared/lib/api/apiServer";
+import { CafeteriaApiResponse } from "@/features/menu/types/cafeteriaType";
+
+export async function getAllCafeteria(): Promise<CafeteriaApiResponse[]> {
+  return await apiServer.get("/api/cafeterias");
+}
