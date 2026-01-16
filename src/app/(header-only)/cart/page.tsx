17fe46441067c84bbe1ce705f9bfb0ce56943b8f
Chuseok22 <bjh59629@naver.com>
2026-01-16 11:02:47 +0900

    장바구니_빈_페이지_개발 : feat : 장바구니 페이지 메뉴 유무에 따른 페이지 랜더링(추후 api요청 로직 분리) https://github.com/CampusTable/campus-table-fe/issues/131

--- src/app/(header-only)/cart/page.tsx
+++ src/app/(header-only)/cart/page.tsx
@@ -1,19 +1,14 @@
 import EmptyCartView from "@/features/cart/components/view/EmptyCartView";
-import CartMenuCard from "@/features/cart/components/card/CartMenuCard";
+import CartView from "@/features/cart/components/view/CartView";
+import { apiServer } from "@/shared/lib/api/apiServer";
+import { CartApiResponse } from "@/features/cart/types/cartType";
+import { toCartInfo } from "@/features/cart/utils/cartMapper";
 
-export default function CartPage() {
-  return (
-    // <>
-    //   <CartMenuCard menuId={1}
-    //                 imageSrc="/tmp/menu/menu-1.png"
-    //                 title="김치제육 덮밥"
-    //                 price={6000} />
-    //   <CartMenuCard menuId={1}
-    //                 imageSrc="/tmp/menu/menu-1.png"
-    //                 title="김치제육 덮밥"
-    //                 rank={1}
-    //                 price={6000} />
-    // </>
-    <EmptyCartView/>
-  );
+export default async function CartPage() {
+
+  const cartApiResponse: CartApiResponse = await apiServer.get("/api/cart");
+  const cartInfo = toCartInfo(cartApiResponse);
+  const empty = cartInfo.totalQuantity === 0;
+
+  return empty ? <EmptyCartView /> : <CartView />
 }
